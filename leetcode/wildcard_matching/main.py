bc57bf4a41a05704c59d3a3e18d8a27c486c3c8f
Ray <lokto.kwan@gmail.com>
2025-12-28 19:42:15 -0500

    update documentation

--- leetcode/wildcard_matching/main.py
+++ leetcode/wildcard_matching/main.py
@@ -1,13 +1,27 @@
 """
-Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+
+ 
 
 Example 1:
-Input: n = 3
-Output: ["((()))","(()())","(())()","()(())","()()()"]
 
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
 Example 2:
-Input: n = 1
-Output: ["()"]
+
+Input: s = "aa", p = "*"
+Output: true
+Explanation: '*' matches any sequence.
+Example 3:
+
+Input: s = "cb", p = "?a"
+Output: false
+Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
 """
 
 def wildcard_matching(s: str, p: str) -> bool:
